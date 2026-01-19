da9144605c11a13005665607e48d5796f4391737
Kojiro ONO <ono.kojiro@gmail.com>
2026-01-19 22:17:53 +0900

    count testcases

--- test/prepare-arp.sh
+++ test/prepare-arp.sh
@@ -7,7 +7,8 @@ arp_opts="${arp_opts} --localnet --ignoredups --plain"
 arp_opts="${arp_opts} -ouifile=/usr/share/arp-scan/ieee-oui.txt"
 arp_opts="${arp_opts} --macfile=/etc/arp-scan/mac-vendor.txt"
 
-echo "1..2"
+
+count=0
 
 rm -rf ${logfile}
 ssh -t abaoaqu "sudo -E arp-scan ${arp_opts}" >> ${logfile}
@@ -16,6 +17,7 @@ if [ "$?" -eq 0 ]; then
 else
   echo "not ok"
 fi
+count=`expr $count + 1`
 
 ssh -t solomon "sudo -E arp-scan ${arp_opts}" >> ${logfile}
 if [ "$?" -eq 0 ]; then
@@ -23,4 +25,15 @@ if [ "$?" -eq 0 ]; then
 else
   echo "not ok"
 fi
+count=`expr $count + 1`
+
+ssh -t xubuntu "sudo -E arp-scan ${arp_opts}" >> ${logfile}
+if [ "$?" -eq 0 ]; then
+  echo "ok"
+else
+  echo "not ok"
+fi
+count=`expr $count + 1`
+
+echo "1..${count}"
 
