1dc329a9c7811319145199adaebff2e1e47eed67
Kojiro ONO <ono.kojiro@gmail.com>
2026-09-21 13:20:00 +0900

    add Makefile

--- examples/run_arpscan.sh
+++ examples/run_arpscan.sh
@@ -9,7 +9,7 @@ arp_opts="${arp_opts} --macfile=/etc/arp-scan/mac-vendor.txt"
 
 rm -rf ${logfile}
 
-targets="luna2:macvlan0 abaoaqu:macvlan0 trixie:enp1s0"
+targets="luna2:macvlan0 luna2:docker0 luna2:virbr0 abaoaqu:macvlan0 trixie:enp1s0"
 
 count=0
 
